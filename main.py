01d62724de6a82af22fa64c8bc392ba4d887680c
life423 <aiandrew0x7f@outlook.com>
2024-10-07 17:50:58 -0700

    add docstrings

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
-# def bmi_calc_metric(weight,height) -> float:
-#     return weight/(height **2)
+def bmi_calc_imperial(weight: float, height: float) -> float:
+    """
+    Calculate BMI using the imperial system.
 
-def bmi_calc_imperial(weight,height) -> float:
-    return (weight * 703)/(height**2)
+    Args:
+        weight (float): Weight in pounds.
+        height (float): Height in inches.
 
+    Returns:
+        float: The calculated Body Mass Index (BMI).
+    """
+    return (weight * 703) / (height ** 2)
 
 def main() -> None:
-    weight: float = 210 
-    height: float = 73
-    # bmi_metric: float = bmi_calc_metric(weight, height)
+    """
+    Main function to gather user input and calculate BMI.
+
+    This function uses hardcoded values for weight and height,
+    calculates BMI using the imperial system, and prints the result.
+    """
+    weight: float = 200 # pounds
+    height: float = 73 #inches
+  
     bmi_imperial: float = bmi_calc_imperial(weight, height)
-    print (f"Here is your BMI ${bmi_imperial: .2f}")
-    
-    
-    
+    print(f"Here is your BMI {bmi_imperial:.2f}")
+
 if __name__ == "__main__":
-  main()
+    main()
